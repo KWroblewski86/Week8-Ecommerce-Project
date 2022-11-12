b928debfa54f59c947b2afccfbfa0d572cd49a90
Kyle Wroblewski <kyle.a.wroblewski@gmail.com>
2022-11-11 18:54:22 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import Home from './component/Home';
+import Navbar from './component/Navbar';
+import Products from './component/Products';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <div>
+          <Navbar />
+
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/products' element={<Products />} />
+
+
+
+          </Routes>
+
+        </div>
+      </Router>
+    </>
   );
 }
 
---
+++ src/component/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Home() {
+
+
+  return (
+
+      <div className="hero">
+          <div className="card bg-dark text-white border-0">
+              <img src="images/bg2.jpg" className="card-img" alt="Background"/>
+                  <div className="card-img-overlay d-flex flex-column justify-content-center">
+                    <div className="container">
+                    <h5 className="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
+                      <p className="card-text lead fs-2">CHECK OUT ALL THE TRENDS</p>
+                    </div>
+                  </div>
+          </div>
+      </div>
+  )
+}
